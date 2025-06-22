d3685534b5e221d3ed0cc8fa79be162a197adec1
PrayagBhakar <prayag.bhakar@gmail.com>
2025-06-22 06:22:47 -0400

    surface location and wifi data

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,25 @@
-import { useNetworkState } from "@uidotdev/usehooks";
-import { MapPin, Wifi, WifiOff } from "lucide-react";
+import { Bot } from "lucide-react";
+import GeoLocationButton from "./geoLocationButton.tsx";
+import NetworkButton from "./networkButton.tsx";
+import { Link } from "@tanstack/react-router";
 
-export function Header() {
-  const network = useNetworkState();
-
+export default function Header() {
   return (
     <header className="navbar bg-base-200 shadow-sm">
       <div className="navbar-start">
         <div className="flex items-center space-x-3">
-          <div role="button" className="btn btn-primary btn-square">
-            311
-          </div>
-          <div role="button" className="btn btn-ghost text-xl font-bold">
+          <Link to="/" role="button" className="btn btn-primary btn-square">
+            <Bot />
+          </Link>
+          <Link to="/" role="button" className="btn btn-ghost text-xl font-bold">
             311 Agent
-          </div>
+          </Link>
         </div>
       </div>
       <div className="navbar-end">
         <div className="flex items-center space-x-2">
-          <div role="button" className="btn btn-primary text-success">
-            <MapPin />
-            <span>NYC</span>
-          </div>
-          <div role="button" className="btn btn-primary btn-square">
-            {network.online ? (
-              <Wifi className="text-success" />
-            ) : (
-              <WifiOff className="text-error" />
-            )}
-          </div>
+          <GeoLocationButton />
+          <NetworkButton />
         </div>
       </div>
     </header>
