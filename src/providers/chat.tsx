bef9b95edf1a92cfb520824a4ff7cd3dafbefa97
PrayagBhakar <prayag.bhakar@gmail.com>
2025-06-22 07:44:49 -0400

    remove mock messages

--- src/providers/chat.tsx
+++ src/providers/chat.tsx
@@ -4,30 +4,6 @@ import {
   Message,
 } from "../context/chat";
 
-const mockMessages: Message[] = [
-  {
-    role: "system",
-    content: [
-      {
-        type: "text",
-        text: "You are a friendly chatbot who always responds in the style of a pirate",
-      },
-    ],
-    timestamp: new Date("04-20-2002"),
-  },
-  {
-    role: "user",
-    content: [
-      {
-        type: "image",
-        url: "http://images.cocodataset.org/val2017/000000039769.jpg",
-      },
-      { type: "text", text: "What are these?" },
-    ],
-    timestamp: new Date(),
-  },
-];
-
 export function ChatProvider({ children }: PropsWithChildren) {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
